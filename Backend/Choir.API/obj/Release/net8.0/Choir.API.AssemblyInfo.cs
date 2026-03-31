4b3fa20a9e9b7ba96e36617208be4da8de1a02cf
jesus love <snowley777@gmail.com>
2026-03-31 10:09:19 -0500

    Updated project files

--- Backend/Choir.API/obj/Release/net8.0/Choir.API.AssemblyInfo.cs
+++ Backend/Choir.API/obj/Release/net8.0/Choir.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Choir.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a1a3b5106e06c5e6f731040ff0cba79f21ecb53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6650e8e475f8fb05544b8a8077bd3835929237d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Choir.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Choir.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
